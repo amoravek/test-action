1b75bf143063ef515cf347850fd8cd8408f3050b
a <amoravek@gmail.com>
2022-04-04 01:36:56 +0200

    updates

---
+++ run-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./scripts/process-downloads.sh \
+artifacts.yaml \
+vendor-metadata.yaml \
+artifactory-user \
+artifactory-password \
+http://artifactory-base-url \
+artifactory-repo
--- scripts/process-downloads.sh
+++ scripts/process-downloads.sh
@@ -1,144 +1,167 @@
 #!/usr/bin/env bash
 
-ARTIFACTS_BATCH_FILE_NAME=artifacts.yaml
-
 set -e +o history
 
-artifactBatchFile=$1
-vendorMetadataFile=$2
-artifactoryUser=$1
-artifactoryPassword=$2
-artifactoryBaseUrl=$3
-artifactoryRepo=$4
+ARTIFACTS_BATCH_FILE=${1:-artifacts.yaml}
+VENDOR_METADATA_FILE=$2
+ARTIFACTORY_USER=$3
+ARTIFACTORY_PASSWORD=$4
+ARTIFACTORY_BASE_URL=$5
+ARTIFACTORY_REPO=$6
 
-function err() {
-    echo "### $1"
+SCRIPT_FILE="$0"
+
+function error() {
+    echo "::error file=${SCRIPT_FILE}::$1"
+}
+
+function warning() {
+    echo "::warning file=${SCRIPT_FILE}::$1"
+}
+
+function info() {
+    echo "::notice file=${SCRIPT_FILE}::$1"
+}
+
+function debug() {
+    echo "::debug file=${SCRIPT_FILE}::$1"
+}
+
+function die() {
+    error "$1"
     exit 1
 }
 
-function msg() {
-    echo "===> $1"
-}
-
 function check_inputs() {
-    if [ -z $artifactBatchFile ]; then
-        err "Missing artifacts batch file ($ARTIFACTS_BATCH_FILE_NAME)"
+    if [ -z $ARTIFACTS_BATCH_FILE ]; then
+        die "Missing artifacts batch file ($ARTIFACTS_BATCH_FILE_NAME)"
     fi
 
-    if [ -z $vendorMetadataFile ]; then
-        err "Missing vendor metadata file"
+    if [ -z $VENDOR_METADATA_FILE ]; then
+        die "Missing vendor metadata file"
     fi
 
-    if [ -z $artifactoryUser ]; then
-        err "Missing Artifactory user"
+    if [ -z $ARTIFACTORY_USER ]; then
+        die "Missing Artifactory user"
     fi
 
-    if [ -z $artifactoryPassword ]; then
-        err "Missing Artifactory password/token"
+    if [ -z $ARTIFACTORY_PASSWORD ]; then
+        die "Missing Artifactory password/token"
     fi
 
-    if [ -z $artifactoryBaseUrl ]; then
-        err "Missing Artifactory base URL"
+    if [ -z $ARTIFACTORY_BASE_URL ]; then
+        die "Missing Artifactory base URL"
     fi
 
-    if [ -z $artifactoryRepo ]; then
-        err "Missing Artifactory repository name"
+    if [ -z $ARTIFACTORY_REPO ]; then
+        die "Missing Artifactory repository name"
     fi
 }
 
 check_inputs
 
-declare -A uriTemplates
-artifactCount=0
+echo "ARTIFACTS_BATCH_FILE: $ARTIFACTS_BATCH_FILE"
+echo "VENDOR_METADATA_FILE: $VENDOR_METADATA_FILE"
+echo "ARTIFACTORY_USER: $ARTIFACTORY_USER"
+echo "ARTIFACTORY_BASE_URL: $ARTIFACTORY_BASE_URL"
+echo "ARTIFACTORY_REPO: $ARTIFACTORY_REPO"
 
-for artifactName in $(yq e '.spec.artifacts | keys' $vendorMetadataFile | awk '{print $2}'); do
-    uriTemplate=$(yq e ".spec.artifacts.${artifactName}.uriTemplate" $vendorMetadataFile)
+declare -A URI_TEMPLATES
 
-    existing=uriTemplates[$artifactName]
-
-    # product name duplicity check
-    if [[ ! -z $existing ]]; then
-        err "Duplicate artifact name \'$artifactName\' in $vendorMetadataFile" 
-    fi
-
-    uriTemplates[$artifactName]=$uriTemplate
-
-    artifactCount=$(($artifactCount+1))
+for ARTIFACT_NAME in $(yq e ".spec.artifacts | keys" $VENDOR_METADATA_FILE | awk '{print $2}'); do
+    URI_TEMPLATE=$(yq e ".spec.artifacts.${ARTIFACT_NAME}.uriTemplate" $VENDOR_METADATA_FILE)
+    URI_TEMPLATES[$ARTIFACT_NAME]=$URI_TEMPLATE
+    debug "URI template: $ARTIFACT_NAME -> $URI_TEMPLATE"
 done
 
-declare -A artifactVersions
+# Retrieve Git tags
+DIR=$(pwd)
+cd $(dirname $VENDOR_METADATA_FILE})
+GIT_TAGS=$(git tag --list)
+cd $DIR
 
-for artifactName in $(yq e '.spec.artifacts | keys' $artifactBatchFile | awk '{print $2}'); do
+declare -A ARTIFACT_VERSIONS
 
-    for version in $(yq e '.spec.artifacts[].${artifactName}' $artifactBatchFile | awk '{print $2}'); do
+for ARTIFACT_NAME in $(yq e ".spec.artifacts | keys" $ARTIFACTS_BATCH_FILE | awk '{print $2}'); do
 
-    productName=$(yq e ".spec.artifacts[$i].name" $artifactBatchFile)
-    version=$(yq e ".spec.artifacts[$i].version" $artifactBatchFile)
-    declaredSha256=$(yq e ".spec.artifacts[$i].sha256" $artifactBatchFile)
-    declaredSha1=$(yq e ".spec.artifacts[$i].sha1" $artifactBatchFile)
-    declaredMd5=$(yq e ".spec.artifacts[$i].md5" $artifactBatchFile)
-    finalUri=$(eval echo "${uriTemplates[$productName]}")
-
-done
-
-
-productVersionsCount=$(yq e '.spec.artifacts | length' $artifactBatchFile)
-
-
-
-i=0
-while [[ $i < $productVersionsCount ]]; do
-    productName=$(yq e ".spec.artifacts[$i].name" $artifactBatchFile)
-    version=$(yq e ".spec.artifacts[$i].version" $artifactBatchFile)
-    declaredSha256=$(yq e ".spec.artifacts[$i].sha256" $artifactBatchFile)
-    declaredSha1=$(yq e ".spec.artifacts[$i].sha1" $artifactBatchFile)
-    declaredMd5=$(yq e ".spec.artifacts[$i].md5" $artifactBatchFile)
-    finalUri=$(eval echo "${uriTemplates[$productName]}")
-
-    echo "===> Downloading '$productName' from '$finalUri' ..."
-
-    checksumHeaders=""
-
-    fileName=$(curl --silent --show-error --fail --head --insecure --location $finalUri | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/' | tr -d '\r')
-    curl --silent --show-error --fail --remote-name --insecure --location $finalUri > /dev/null
-
-    if [ ! $? -eq 0 ]; then
-        echo "### Error downloading '$productName' ($fileName)"
-        continue
+    if [[ "x${URI_TEMPLATES[$ARTIFACT_NAME]}" == "x" ]]; then
+        die "Missing URI pattern for artifact $ARTIFACT_NAME"
     fi
 
-    if [[ $declaredSha != "null" ]]; then
-        echo -n "Verifying checksum ... "
-        sha256Sum=$(sha256sum "$fileName" | cut -d " " -f 1)
-        sha1Sum=$(sha1sum "$fileName" | cut -d " " -f 1)
-        md5Sum=$(md5sum "$fileName" | cut -d " " -f 1)
+    VERSIONS_COUNT=$(yq e ".spec.artifacts.${ARTIFACT_NAME} | length" $ARTIFACTS_BATCH_FILE)
+    i=0
 
-        if [[ "$declaredSha" != "$sha256Sum" ]]; then
-            echo "### Invalid checksum - $productName:$version ($fileName): declared: [$declaredSha] real: [$sha]"
+    while [ $i -ne $VERSIONS_COUNT ]; do
+        VERSION=$(yq e ".spec.artifacts.${ARTIFACT_NAME}[$i].version" $ARTIFACTS_BATCH_FILE)
+        DECLARED_SHA256=$(yq e ".spec.artifacts.${ARTIFACT_NAME}[$i].sha256" $ARTIFACTS_BATCH_FILE)
+        DECLARED_SHA1=$(yq e ".spec.artifacts.${ARTIFACT_NAME}[$i].sha1" $ARTIFACTS_BATCH_FILE)
+        DECLARED_MD5=$(yq e ".spec.artifacts.${ARTIFACT_NAME}[$i].md5" $ARTIFACTS_BATCH_FILE)
+        FINAL_URI=$(eval echo "${URI_TEMPLATES[$ARTIFACT_NAME]}")
+
+        if [[ $GIT_TAGS == *"${ARTIFACT_NAME}_${VERSION}"* ]]; then
+            info "Artifact $ARTIFACT_NAME $VERSION already processed; skipping"
             continue
-        else
-            echo "OK"
-            checksumHeaders="--header X-Checksum-Sha256:${sha256Sum} --header X-Checksum-Sha1:${sha1Sum} --header X-Checksum:${md5Sum}"
-            # checksumHeaders="--header X-Checksum-Sha256:${declaredSha}"
         fi
-    else
-        echo "No checksum provided to verify"
-    fi
 
-    base=$(basename -- "$fileName")
-    ext="${base#*.}"
-    name="${base%%.*}"
+        info "===> Downloading $ARTIFACT_NAME $VERSION from $FINAL_URI ..."
 
-    if [[ "$name" == "$ext" ]]; then
-        targetFileName=$name-$version
-    else
-        targetFileName=$name-$version.$ext
-    fi
-
-    targetUrl="${artifactoryBaseUrl}/${artifactoryRepo}/${targetFileName}"
-    echo "Uploading to $targetUrl ... "
-    curl --silent --show-error --fail --insecure --user ${artifactoryUser}:${artifactoryPassword} ${checksumHeaders} --request PUT "$targetUrl" --upload-file ${fileName} > /dev/null
-
-    i=$(($i+1))
+        i=$(($i+1))
+    done
 done
+
+# i=0
+# while [[ $i < $productVersionsCount ]]; do
+#     productName=$(yq e ".spec.artifacts[$i].name" $ARTIFACTS_BATCH_FILE)
+#     version=$(yq e ".spec.artifacts[$i].version" $ARTIFACTS_BATCH_FILE)
+#     declaredSha256=$(yq e ".spec.artifacts[$i].sha256" $ARTIFACTS_BATCH_FILE)
+#     declaredSha1=$(yq e ".spec.artifacts[$i].sha1" $ARTIFACTS_BATCH_FILE)
+#     declaredMd5=$(yq e ".spec.artifacts[$i].md5" $ARTIFACTS_BATCH_FILE)
+#     finalUri=$(eval echo "${URI_TEMPLATES[$productName]}")
+
+#     info "===> Downloading '$productName' from '$finalUri' ..."
+
+#     checksumHeaders=""
+
+#     fileName=$(curl --silent --show-error --fail --head --insecure --location $finalUri | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/' | tr -d '\r')
+#     curl --silent --show-error --fail --remote-name --insecure --location $finalUri > /dev/null
+
+#     if [ ! $? -eq 0 ]; then
+#         warning "### Error downloading '$productName' ($fileName)"
+#         continue
+#     fi
+
+#     if [[ $declaredSha != "null" ]]; then
+#         echo -n "Verifying checksum ... "
+#         sha256Sum=$(sha256sum "$fileName" | cut -d " " -f 1)
+#         sha1Sum=$(sha1sum "$fileName" | cut -d " " -f 1)
+#         md5Sum=$(md5sum "$fileName" | cut -d " " -f 1)
+
+#         if [[ "$declaredSha" != "$sha256Sum" ]]; then
+#             warning "### Invalid checksum - $productName:$version ($fileName): declared: [$declaredSha] real: [$sha]"
+#             continue
+#         else
+#             echo "OK"
+#             checksumHeaders="--header X-Checksum-Sha256:${sha256Sum} --header X-Checksum-Sha1:${sha1Sum} --header X-Checksum:${md5Sum}"
+#             # checksumHeaders="--header X-Checksum-Sha256:${declaredSha}"
+#         fi
+#     else
+#         info "No checksum provided to verify"
+#     fi
+
+#     base=$(basename -- "$fileName")
+#     ext="${base#*.}"
+#     name="${base%%.*}"
+
+#     if [[ "$name" == "$ext" ]]; then
+#         targetFileName=$name-$version
+#     else
+#         targetFileName=$name-$version.$ext
+#     fi
+
+#     targetUrl="${ARTIFACTORY_BASE_URL}/${ARTIFACTORY_REPO}/${targetFileName}"
+#     info "Uploading to $targetUrl ... "
+#     curl --silent --show-error --fail --insecure --user ${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} ${checksumHeaders} --request PUT "$targetUrl" --upload-file ${fileName} > /dev/null
+
+#     i=$(($i+1))
+# done
 
